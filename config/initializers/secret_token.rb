c65d90cc21643b661201e201a2797205299f4dad
Julianne Beaudoin <jubeaudoin@gmail.com>
2013-04-28 21:01:24 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoTwitter::Application.config.secret_token = '2909c6d9da471d94fb8d7f0f71b5ee5405c1ab4febcee839479cfa67885a64f88473073c80dad8452d4a5041038ad664fa6de1722e8bb8d8e4fbf0b5652abbdf'
